f9fb0f39ce6ed78178e5fa65af3f8cc0e1ac65df
Fredrik Welander <welandfr@arcada.fi>
2026-03-27 11:31:31 +0200

    add hello route

--- app/main.py
+++ app/main.py
@@ -4,9 +4,9 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return { "msg": "Morjens!", "v": "0.2" }
+    return { "msg": "Hello local docker"}
 
 
-@app.get("/items/{id}")
-def read_item(item_id: int, q: str = None):
-    return {"id": id, "q": q}
+@app.get("/hello")
+def hello():
+    return { "msg": "Hello fredde"}
